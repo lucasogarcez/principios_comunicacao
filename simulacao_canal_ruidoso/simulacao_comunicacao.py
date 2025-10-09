087a03695816b04c62688500fcbd3e2d8718fc5d
Lucas Oliveira Garcez <estudosengcomputacao@gmail.com>
2025-10-09 08:34:00 -0300

    refactor: geração aleatória do sinal
    Função "gerar sinal" modificada para que, a cada vez que a página for recarregada (sem influência do slider do Plotly), seja gerado um novo sinal para simulação.

--- simulacao_canal_ruidoso/simulacao_comunicacao.py
+++ simulacao_canal_ruidoso/simulacao_comunicacao.py
@@ -3,8 +3,8 @@ import numpy as np
 import plotly.graph_objects as go
 
 # Gera um sinal binário
-def gerar_sinal(n_bits):
-    rng = np.random.default_rng(42)
+def gerar_sinal(n_bits, seed):
+    rng = np.random.default_rng(seed)
     return rng.integers(0, 2, n_bits)
 
 # Adiciona ruído Gaussiano ao sinal
@@ -29,10 +29,13 @@ def calcular_snr(sinal_original, sinal_recebido):
 # Interface do Streamlit
 st.title("Simulação de Comunicação com Canal Ruidoso (BWSG)")
 
-n_bits = st.slider("Número de bits", 10, 1000, 100, step=10)
+n_bits = st.slider("Número de bits", 10, 100, 50, step=10)
 desvio_padrao = st.slider("Intensidade do Ruído (Desvio Padrão)", 0.0, 3.0, 0.5, step=0.1)
 
-sinal = gerar_sinal(n_bits)
+if 'seed' not in st.session_state:
+    st.session_state.seed = np.random.randint(0, 10000)
+
+sinal = gerar_sinal(n_bits, st.session_state.seed)
 sinal_ruidoso = adicionar_ruido(sinal, desvio_padrao)
 
 ber = calcular_ber(sinal, sinal_ruidoso)
